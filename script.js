fd08b3e0febfce20eb251e33e35675b46efdf609
Davboi400 <davidochola2013@gmail.com>
2026-07-04 20:40:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
-let board;
-let whiteTurn;
-let selectedSquare;
-let legalMoves;
+const game = {
+
+    board: [],
+
+    whiteTurn: true,
+
+    selectedSquare: null,
+
+    legalMoves: [],
+
+    history: []
+
+};
 
 function startGame() {
 
-    board = [
+    game.board = [
+
         ["r","n","b","q","k","b","n","r"],
         ["p","p","p","p","p","p","p","p"],
+
         ["","","","","","","",""],
+
         ["","","","","","","",""],
+
         ["","","","","","","",""],
+
         ["","","","","","","",""],
+
         ["P","P","P","P","P","P","P","P"],
         ["R","N","B","Q","K","B","N","R"]
+
     ];
 
-    whiteTurn = true;
-    selectedSquare = null;
-    legalMoves = [];
+    game.whiteTurn = true;
+    game.selectedSquare = null;
+    game.legalMoves = [];
+    game.history = [];
 
     drawBoard();
+
 }
 
 startGame();
